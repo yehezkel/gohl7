2cb476ff3dcef620b5ce0bb06cecba1e6bdd7cef
Yehezkel Reyes <yehezkel.rp@gmail.com>
2019-02-11 18:04:40 -0500

    Validators mockup

---
+++ validators.go
@@ -0,0 +1,29 @@
+package gohl7
+
+import (
+	"errors"
+)
+
+var (
+//ErrEmptyChildren = errors.New("No child to pop off")
+)
+
+func SegmentValidator(parent, child Field) error {
+
+	return nil
+}
+
+func ComponentValidator(parent, child Field) error {
+
+	return nil
+}
+
+func RepeatedValidator(parent, child Field) error {
+
+	return nil
+}
+
+func SubComponentValidator(parent, child Field) error {
+
+	return nil
+}
