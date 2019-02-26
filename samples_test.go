f59739b4d627d9e7975f025a4825d80a0e9267d1
Yehezkel Reyes <yehezkel.rp@gmail.com>
2019-02-25 19:14:33 -0500

    Adding utility functions for testing

--- samples_test.go
+++ samples_test.go
@@ -13,6 +13,18 @@ func TestSimpleMessage(t *testing.T) {
 
 	raw := []byte("MSH|^~\\&|aaa|bbbb\rTMP|123|456")
 
+    build := newComplexFieldWithChild(
+        message, MessageValidator,
+
+        newComplexFieldWithChild(segment,SegmentValidator,
+            newSimpleStr("MSH"),newSimpleStr("^~\\&"), newSimpleStr("aaa"), newSimpleStr("bbbb"),
+        ),
+
+        newComplexFieldWithChild(segment,SegmentValidator,
+            newSimpleStr("TMP"),newSimpleStr("123"), newSimpleStr("456"),
+        ),
+    )
+
 	parser, err := NewHl7Parser(raw)
 
 	if err != nil {
@@ -25,7 +37,10 @@ func TestSimpleMessage(t *testing.T) {
 		log.Panic(err)
 	}
 
-	log.Printf("%s\n", msg.ComplexField)
+    err = deepEqual(msg.ComplexField,build)
+    if err != nil {
+        log.Panic(err)
+    }
 }
 
 func TestSimpleRepeated(t *testing.T) {
@@ -68,7 +83,7 @@ func TestSimpleComponentMessage(t *testing.T) {
 
 func TestRepatedComponentMessage(t *testing.T) {
 
-	raw := []byte("MSH|^~\\&|aaa|rrr1~bbb1^bbb2^bbb3")
+	raw := []byte("MSH|^~\\&|aaa|rrr1~bbb1^bbb2^bbb3~ccc1\r")
 
 	parser, err := NewHl7Parser(raw)
 
---
+++ utils.go
@@ -0,0 +1,67 @@
+package gohl7
+
+import (
+    "errors"
+)
+
+var (
+    errNoMatchingTypes = errors.New("No matching type")
+    errNonInternalImplementation = errors.New("Provided fields do not match internal implementation")
+    errNonMatchingChildren = errors.New("Different amount of childrens")
+)
+
+//function newSimpleStr is an internal utility function
+//mostly for testing
+func newSimpleStr(s string) *SimpleField {
+
+    return NewSimpleField([]byte(s))
+}
+
+
+// function newComplexFieldWithChild is an internal utility function
+// mostly for testing
+//error code is ignore on purpose as its assume the call is internal
+func newComplexFieldWithChild(t FieldType, v func(Field, Field) error, children ...Field) *ComplexField {
+
+    complexF := NewComplexField(t,v)
+
+    for _, child := range children {
+        _ = complexF.Push(child)
+    }
+
+    return complexF
+}
+
+//function deepEqual compare two fields recursivly for equality
+//a return value of nil implies equal a non nil implies difference
+func deepEqual(l,r Field) (error) {
+
+    if l.Type() != r.Type() {
+        return errNoMatchingTypes
+    }
+
+    if l.Type() == Simple {
+        return nil
+    }
+
+    complexL,okL := l.(*ComplexField)
+    complexR,okR := r.(*ComplexField)
+
+    if !okL || !okR {
+        return errNonInternalImplementation
+    }
+
+    childrenL, childrenR := complexL.children, complexR.children
+    if len(childrenL) != len(childrenR) {
+        return errNonMatchingChildren
+    }
+
+    for i:=0; i < len(childrenL); i++ {
+        err := deepEqual(childrenL[i],childrenR[i])
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
